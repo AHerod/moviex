07a00acbed8be5c233cfdf926974aab1e17e05d0
agaherod <aherod@wearevirtua.com>
2021-03-01 16:18:40 +0100

    basic setup

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '829eb129aac42fcb401121453debcd2f'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5288cd74-f8fb-4488-9c7f-27b4951d487f'
+};
